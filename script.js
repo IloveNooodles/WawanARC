3d1f4d4778439a24dcc81d5e06d3c3fbe1607ac1
Muhammad Garebaldhie Er Rahman <63847012+IloveNooodles@users.noreply.github.com>
2021-03-09 22:22:59 +0700

    Final website

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementsByClassName("btn");
+
+for (let i = 0; i < btn.length; i++){
+    btn[i].addEventListener("click", ()=>{
+        let more = document.getElementById("more"+(i+1))
+        if(getComputedStyle(more).display === "none"){
+            more.style.display = "inline";
+            btn[i].innerText = "Read Less";
+        } else {
+            more.style.display = "none";
+            btn[i].innerText = "Read More";
+        }
+    });
+}
